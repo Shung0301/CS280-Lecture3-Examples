ca4a7603d51df6a30de0ed33b292f6a2a9869be6
Shung0301 <tylertangbu@gmail.com>
2017-03-10 20:04:51 +0800

    第一段落先行上傳

---
+++ Example01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CS280_Lecture1_Examples;
+
+namespace Example01
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //一維陣列
+            Pokemon[] PokemonList = new Pokemon[3];
+            PokemonList[0] = new Pokemon();
+            PokemonList[0].Weight = 2.0f;
+            PokemonList[1] = new Pokemon();
+            PokemonList[2] = new Pokemon();
+        }
+    }
+}
